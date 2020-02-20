9dce5ed1641745084d6706d7c2c01994e2b66242
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-02-20 20:14:10 +0000

    Created files architecture and configured Webpack for development.

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js", //entry for main js file
+  output: {
+    // output instead of app/dist/main.js
+    filename: "bundeled.js",
+    path: path.resolve(__dirname, "app") //webpack requires absolute path
+  },
+  mode: "development", //
+  watch: true // webpack stays running and re-boundels on save
+};
